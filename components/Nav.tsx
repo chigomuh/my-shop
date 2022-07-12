3f4d4e0db74475f4be2272b5023fcb36ba64e3ed
chigosix <tngusrldnjs1@gmail.com>
2022-07-13 01:05:45 +0900

    1. 로그인, 회원가입 모달 구현

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -12,6 +12,8 @@ import {
 const Nav = () => {
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
   const [cartOpen, setCartOpen] = useState<boolean>(false);
+  const [loginOpen, setLoginOpen] = useState<boolean>(false);
+  const [signUpOpen, setSignUpOpen] = useState<boolean>(false);
   const state = useCartReducerState();
   const dispatch = useCartReducerDispatch();
 
@@ -31,6 +33,32 @@ const Nav = () => {
     setCartOpen(!cartOpen);
 
     if (!cartOpen) {
+      document.body.setAttribute("style", "overflow: hidden;");
+    } else {
+      document.body.setAttribute("style", "");
+    }
+  };
+
+  const handleLoginTap = () => {
+    if (signUpOpen) {
+      setSignUpOpen(!signUpOpen);
+    }
+    setLoginOpen(!loginOpen);
+
+    if (!loginOpen) {
+      document.body.setAttribute("style", "overflow: hidden;");
+    } else {
+      document.body.setAttribute("style", "");
+    }
+  };
+
+  const handleSignUpTap = () => {
+    if (loginOpen) {
+      setLoginOpen(!loginOpen);
+    }
+    setSignUpOpen(!signUpOpen);
+
+    if (!signUpOpen) {
       document.body.setAttribute("style", "overflow: hidden;");
     } else {
       document.body.setAttribute("style", "");
@@ -110,17 +138,14 @@ const Nav = () => {
               </div>
             ) : null}
           </div>
-          <div>
-            <Link href="/login">
-              <a className="bg-blue-300 rounded-full flex items-center hover:cursor-pointer">
-                <Image
-                  src="/image/profile-icon.png"
-                  alt="profile"
-                  width={32}
-                  height={32}
-                />
-              </a>
-            </Link>
+          <div className="bg-blue-300 rounded-full flex items-center hover:cursor-pointer">
+            <Image
+              src="/image/profile-icon.png"
+              alt="profile"
+              width={32}
+              height={32}
+              onClick={handleLoginTap}
+            />
           </div>
           <div
             className="flex lg:hidden items-center hover:cursor-pointer"
@@ -207,17 +232,14 @@ const Nav = () => {
                     </div>
                   ) : null}
                 </div>
-                <div>
-                  <Link href="/login">
-                    <a className="bg-blue-300 rounded-full flex items-center hover:cursor-pointer">
-                      <Image
-                        src="/image/profile-icon.png"
-                        alt="profile"
-                        width={32}
-                        height={32}
-                      />
-                    </a>
-                  </Link>
+                <div className="bg-blue-300 rounded-full flex items-center hover:cursor-pointer">
+                  <Image
+                    src="/image/profile-icon.png"
+                    alt="profile"
+                    width={32}
+                    height={32}
+                    onClick={handleLoginTap}
+                  />
                 </div>
               </div>
             </div>
@@ -345,6 +367,140 @@ const Nav = () => {
                     PROCEED TO CHECKOUT
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+      {loginOpen && (
+        <>
+          <div
+            className="absolute top-0 left-0 w-screen h-screen bg-black opacity-20 z-40"
+            onClick={handleLoginTap}
+          ></div>
+          <div className="absolute top-1/2 left-1/2 w-96 h-96 bg-white z-50 -translate-x-1/2 translate-y-1/2">
+            <div className="absolute top-0 right-0 m-4 cursor-pointer hover:opacity-50">
+              <Image
+                src="/image/exit.svg"
+                alt="exit-icon"
+                width={32}
+                height={32}
+                onClick={handleLoginTap}
+              />
+            </div>
+            <div className="w-full flex justify-center">
+              <div className="w-4/6">
+                <div className="flex justify-center m-10">
+                  <Image
+                    src="/image/logo.svg"
+                    alt="vercel-icon"
+                    width={60}
+                    height={60}
+                  />
+                </div>
+                <form className="w-full space-y-4">
+                  <input
+                    type="text"
+                    className="w-full h-10 border border-black px-4"
+                    placeholder="Email"
+                    autoFocus
+                  />
+                  <input
+                    type="password"
+                    className="w-full h-10 border border-black px-4"
+                    placeholder="Password"
+                  />
+                  <div className="bg-black w-full h-10 text-white text-center flex items-center justify-center font-bold text-sm cursor-pointer">
+                    Log In
+                  </div>
+                  <div className="text-sm flex justify-center items-center">
+                    Don&apos;t have an account?&nbsp;
+                    <b
+                      className="cursor-pointer hover:underline"
+                      onClick={handleSignUpTap}
+                    >
+                      Sign Up
+                    </b>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+      {signUpOpen && (
+        <>
+          <div
+            className="absolute top-0 left-0 w-screen h-screen bg-black opacity-20 z-40"
+            onClick={handleSignUpTap}
+          ></div>
+          <div className="absolute top-full left-1/2 w-96 h-max bg-white z-50 -translate-x-1/2 pb-8">
+            <div className="absolute top-0 right-0 m-4 cursor-pointer hover:opacity-50">
+              <Image
+                src="/image/exit.svg"
+                alt="exit-icon"
+                width={32}
+                height={32}
+                onClick={handleSignUpTap}
+              />
+            </div>
+            <div className="w-full flex justify-center">
+              <div className="w-4/6">
+                <div className="flex justify-center m-10">
+                  <Image
+                    src="/image/logo.svg"
+                    alt="vercel-icon"
+                    width={60}
+                    height={60}
+                  />
+                </div>
+                <form className="w-full space-y-4">
+                  <input
+                    type="text"
+                    className="w-full h-10 border border-black px-4"
+                    placeholder="First Name"
+                    autoFocus
+                  />
+                  <input
+                    type="password"
+                    className="w-full h-10 border border-black px-4"
+                    placeholder="Last Name"
+                  />
+                  <input
+                    type="text"
+                    className="w-full h-10 border border-black px-4"
+                    placeholder="Email"
+                  />
+                  <input
+                    type="password"
+                    className="w-full h-10 border border-black px-4"
+                    placeholder="Password"
+                  />
+                  <div className="flex text-sm items-center">
+                    <div>
+                      <Image
+                        src="/image/notice.svg"
+                        alt="notice-icon"
+                        width={16}
+                        height={16}
+                      />
+                      <b>Info</b>: Passwords must be longer than 7 chars and
+                      include numbers.
+                    </div>
+                  </div>
+                  <div className="bg-black w-full h-10 text-white text-center flex items-center justify-center font-bold text-sm cursor-pointer">
+                    Sign Up
+                  </div>
+                  <div className="text-sm flex justify-center items-center">
+                    Do you have an account?&nbsp;
+                    <b
+                      className="cursor-pointer hover:underline"
+                      onClick={handleLoginTap}
+                    >
+                      Log In
+                    </b>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
